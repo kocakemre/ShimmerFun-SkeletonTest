269754d7ad75f2c7bef413fd54ce031076115f97
EMRE KOCAK <emre.kocak@topkapidanismanlik.com.tr>
2023-02-21 10:51:03 +0300

    Initial Commit

---
+++ ShimmerFun-SkeletonTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShimmerFun-SkeletonTest
+//
+//  Created by Emre Koçak on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
